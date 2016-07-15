fd4a41426839999df56edab7e2161b88ea823011
talyerushalmi <tal.y3rushalmi@gmail.com>
2016-07-16 01:29:41 +0300

    Server now receives client's POST requests + fixes and upgrades.

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
 var http = require('http');
 var fs = require('fs');
+var qs = require('querystring');
+
 var server = http.createServer(function(req, res) {
-	displayForm(res);
+	if (req.method == 'GET') {
+		console.log("GET");
+		displayForm(res);
+	}
+	else if (req.method == 'POST') {
+		console.log("POST");
+		processSubmission(req, res);
+	}
 });
 
 function displayForm(res) {
+	// handling the GET requests
 	fs.readFile('index.html', function(err, data) {
 		res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
 		res.write(data);
 		res.end();
 	});
+}
+
+function processSubmission(req, res) {
+	// handling the POST requests
+	
+	var body = '';
+
+        req.on('data', function (data) {
+		body += data;
+		// Kill connection in case data is too big (to prevent DOS)
+		// 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 = ~1MB
+		if (body.length > 1e6)
+			req.connection.destroy();
+	});
+
+        req.on('end', function () {
+		var post = qs.parse(body);
+		var urlInput = post.urlInput;
+		if (urlInput.length != 0) {
+			console.log("Received data: " + urlInput);
+		}
+        });
+
+	displayForm(res);
 }
 
 server.listen(8080);
