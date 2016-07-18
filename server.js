53005787c5c2e8b90aef13afb09f12c6eb1d6675
talyerushalmi <tal.y3rushalmi@gmail.com>
2016-07-18 22:37:21 +0300

    Added HTTP GET resource request system + UNTESTED rsmq sender.

--- server.js
+++ server.js
@@ -1,20 +1,58 @@
 var http = require('http');
 var fs = require('fs');
 var qs = require('querystring');
+var RedisSMQ = require('rsmq');
+var url = require("url");
+var path = require("path");
+
+var HOST = '127.0.0.1';
+var PORT = 6379;
+var NAMESPACE = 'rsmq';
+rsmq = new RedisSMQ({host: HOST, port: PORT, ns: NAMESPACE});
 
 var server = http.createServer(function(req, res) {
 	if (req.method == 'GET') {
-		console.log("GET");
-		displayForm(res);
+		console.log('GET');
+		displayForm(req, res);
 	}
 	else if (req.method == 'POST') {
-		console.log("POST");
+		console.log('POST');
 		processSubmission(req, res);
 	}
 });
 
-function displayForm(res) {
+function displayForm(req, res) {
 	// handling the GET requests
+
+	var uri = url.parse(req.url).pathname;
+	var filename = path.join(process.cwd(), uri);
+
+	fs.exists(filename, function(exists) {
+		if(!exists) {
+			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.write("404 Not Found\n");
+			res.end();
+			return;
+		}
+
+		if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+		fs.readFile(filename, "binary", function(err, file) {
+			if(err) {        
+				res.writeHead(500, {"Content-Type": "text/plain"});
+				res.write(err + "\n");
+				res.end();
+				return;
+			}
+
+			res.writeHead(200);
+			res.write(file, "binary");
+			res.end();
+		});
+	});
+
+	return;
+
 	fs.readFile('index.html', function(err, data) {
 		res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
 		res.write(data);
@@ -39,12 +77,22 @@ function processSubmission(req, res) {
 		var post = qs.parse(body);
 		var urlInput = post.urlInput;
 		if (urlInput.length != 0) {
-			console.log("Received data: " + urlInput);
+			sendToUntestedQueue(urlInput);
 		}
         });
 
-	displayForm(res);
+	displayForm(req, res);
+}
+
+function sendToUntestedQueue(urlInput) {
+	// send the urlInput to the untested URLs queue
+	// THEORETICALLY FINISHED BUT UNTESTED !!!! <----------------------------------------------------------------------------------------
+	rsmq.sendMessage({qname:"untestedQueue", message: urlInput}, function (err, resp) {
+		if (resp) {
+			console.log("Message sent. ID:", resp);
+		}
+	});
 }
 
 server.listen(8080);
-console.log("Server in running on port 8080");
+console.log('Server in running on port 8080');
