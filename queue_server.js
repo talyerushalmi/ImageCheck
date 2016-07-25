9046c36345e222184f4b45cf269f8b6a0a765e65
talyerushalmi <tal.y3rushalmi@gmail.com>
2016-07-25 15:48:58 +0300

    on queue_server.js needs to fix waterfall architecture and while loop

--- queue_server.js
+++ queue_server.js
@@ -1,4 +1,6 @@
+var async = require('async');
 var RedisSMQ = require('rsmq');
+var request = require('request');
 
 var HOST = '127.0.0.1';
 var PORT = 6379;
@@ -23,7 +25,7 @@ var redisOptions = {
 };
 var rsmq = new RedisSMQ({host: HOST, port: PORT, ns: NAMESPACE, options: redisOptions});
 
-function init() {
+function init(callback) {
 	rsmq.createQueue({qname:'untestedQueue'}, function (err, resp) {
 		// create 'untestedQueue' queue
 		if (resp === 1) {
@@ -53,6 +55,8 @@ function init() {
 			console.log('Count not create notImageQueue - maybe it already exists?');
 		}
 	});
+
+	callback(null)
 }
 
 function receiveUrl() {
@@ -75,7 +79,7 @@ function receiveUrl() {
 			}
 			// remove the URL from untestedQueue
 			rsmq.deleteMessage({qname:'untestedQueue', id: resp.id}, function (err, resp) {
-				if (resp===1) {
+				if (resp === 1) {
 					console.log('Message ', resp.id, ' deleted from untestedQueue.');
 				}
 			});
@@ -85,22 +89,59 @@ function receiveUrl() {
 }
 
 function checkUrl(urlInput) {
-	// temporary, just to test the function
-	if (urlInput === 'image')
-		return true;
-	return false;
-	// end of temp part
+	// image format magic numbers from here: http://www.astro.keele.ac.uk/oldusers/rno/Computing/File_magic.html
+	var magicNums = {
+		bmp: '424d',
+		fits: '53494d504c45',
+		gif: '47494638',
+		gks: '474b534d',
+		rgb: '01da',
+		itc: 'f10040bb',
+		jpg: 'ffd8ffe0',
+		nif: '49494e31',
+		pm: '56494557',
+		png: '89504e47',
+		ps: '2521',
+		ras: '59a66a95',
+		tga: 'xxxxxx',
+		tif: '4d4d002a',
+		tif2: '49492a00',
+		xbm: 'xxxx',
+		xcf: '67696d70207863662076',
+		fig: '23464947',
+		xpm: '2f2a2058504d202a2f'
+	};
+	request(options, function (err, response, body) {
+		if(!err && response.statusCode == 200){
+			for (var format in magicNums) {
+				if (magicNums[format] == body.toString('hex', 0, magicNums[format].length / 2)) // check for matches
+					return true; // found match
+			}
+		}
+	});
 
-	// test wether the given urlInput holds an image or not.
-	// WORK ON ME! <------------------------------------------------------------------------------------------------------------
+	return false; // no matches were found
+}
+
+function loop(callback) {
+	// loop on receiveUrl
+	console.log('Entered loop.');
+	// the loops causes a heap overflow, each iteration should finish before continuing <---------------------------------------
+	while (true) {
+		receiveUrl()
+	}
+
+	callback(null);
 }
 
 function main() {
-	// the program gets to the loop before calling init(), NEEDS FIX <----------------------------------------------------------
-	init();
-	while (true) {
-		receiveUrl();
-	}
+	// the program gets to the loop before calling init(), fix waterfall architecture <-----------------------------------------
+	async.waterfall([
+		init,
+		loop
+	], function (err, result) {
+		console.log('Exited loop.');
+	});
 }
 
 main();
